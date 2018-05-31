46a3dd6d8a8e1e527e64ae5550d49edc30c35486
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-05-31 16:51:57 +0200

    [src] Update Alimentation pic

--- hardware/Alimentation_18F14k22.c
+++ hardware/Alimentation_18F14k22.c
@@ -30,14 +30,13 @@ Hardware:
 
 Programme pour commander l'alimentation générale d'un système (via un MOSFET de
 puissance IPB80P03P4L) à partir d'un ILS connecté sur l'entrée INT2, le programme
-contrôle la tension d'alimentation de 4 batteries, la valeur est retourner sur 
+contrôle la tension d'alimentation de 4 batteries, la valeur est retournée sur 
 ordre via l'I2C.
 La sortie RA4 commande trois LED de repérage via le circuit ZXLD1350.
 
 14/03/18/ Implantation et essai du programme, seuil des batteries à corriger
 
 */
-
 
 sbit BAT1 at LATC.B0; // entrée de controle de tension BAT1
 sbit BAT2 at LATC.B1; // entrée de controle de tension BAT2
@@ -50,17 +49,13 @@ sbit LED at LATA.B0; // sortie LED
 sbit LED_PUISSANCE at LATA.B4; // sortie LED de puissance
 sbit ALIM at LATA.B5; // sortie MOSFET de puissance, commande de l'alimentation
 
-#define QUANTUM 0.015625;
-#define WARNING_LOW_VOLTAGE 10.4
+// I2C
 #define ADDRESS_I2C 0x72;
-
-unsigned short Default = 0;
-unsigned short POWER = 0;
-
-unsigned short compteur1 = 0;
-unsigned short compteur2 = 0;
-unsigned short compteur3 = 0;
-unsigned short i = 0;
+#define SIZE_RX_BUFFER 3
+unsigned short rxbuffer_tab[SIZE_RX_BUFFER];
+unsigned short tmp_rx = 0;
+unsigned short nb_tx_octet = 0;
+unsigned short nb_rx_octet = 0;
 
 // ILS
 #define ILS_CPT_TIME 4
@@ -68,96 +63,72 @@ unsigned short ils_cpt = 4;
 bool ils_removed = true;
 
 // State Machine
-enum power_state {IDLE,POWER_ON,SLEEP};
+enum power_state {IDLE,POWER_ON,WAIT_TO_SLEEP, SLEEP};
 
 // Batteries
+#define WARNING_LOW_VOLTAGE 665 // 0.015625 (quantum) / 10.4 (min tension)
 unsigned int battery_voltage[4];
-bool battery_voltage_default[4];
 bool battery_global_default = false;
 
-// I2C
-#define SIZE_RX_BUFFER 3
-unsigned short rxbuffer_tab[SIZE_RX_BUFFER];
-unsigned short j = 0;
-unsigned short nb_tx_octet = 0;
-unsigned short nb_rx_octet = 0;
-
 // Flasher LED
+bool start_led_puissance = false;
 unsigned short led_puissance_delay = 20; // 100ms * val (default = 2s)
 unsigned short cpt_led_puissance = 20;
 
+// Led
+unsigned short led_delay = 100;
+unsigned short cpt_led = 100;
+bool set_led_on = false;
 
-/**************************************************************************************************
-* Fonction pour convertir les données I2C transmises du maître
-* Le maître transmet trois octets à suivre:
-*
-*
-* exemple: le maitre transmet les trames suivantes
+// Sleep mode
+unsigned char time_to_start[3] = {1, 0, 0}; // hour, min, sec
+unsigned char time_to_stop = 60; // in sec (max 255 sec)
 
-Consigne on met en marche la sortie LED_PUISSANCE (LED_PUISSANCE=1):
-I2C1_Start();
-I2C1_Write(0x72);   // send byte via I2C  (device address + W) adresse 0x72
-I2C1_Write(0x00);  //
-I2C1_Write(0x02);  //
-I2C1_Write(0x01);  //
-I2C1_Stop();
+unsigned char default_time_to_start[3] = {1, 0, 0};
+unsigned char default_time_to_stop = 60;
 
-Consigne on coupe la sortie LED_PUISSANCE (LED_PUISSANCE=0):
-I2C1_Start();
-I2C1_Write(0x72);   // send byte via I2C  (device address + W) adresse 0x72
-I2C1_Write(0x00);  //
-I2C1_Write(0x02);  //
-I2C1_Write(0x00);  //
-I2C1_Stop();
-
-
-i2cdetect -y 2 pour la pcduino
-
-adresse du circuit 0x72 --> 0111 0010
-un i2cget ou un i2cset postionne le dernier bit à 1, suivant une lecture ou écriture
-
-0011 1001 --> 0x39
-
-i2cset -y 2 0x39 0x00 0x01 0x00 i --> on coupe l'alimentation générale ALIM (ALIM=0)
-i2cset -y 2 0x39 0x00 0x02 0x00 i --> on coupe la sortie LED_PUISSANCE (LED_PUISSANCE=0)
-i2cset -y 2 0x39 0x00 0x02 0x01 i --> on met en marche la sortie LED_PUISSANCE (LED_PUISSANCE=0)
-
-
-Exemple de séquence en simulation sous Proteus:
-
-S 0x72 0x00 0x01 0x00 P --> on coupe l'alimentation générale ALIM (ALIM=0)
-S 0x72 0x00 0x02 0x00 P --> on coupe la sortie LED_PUISSANCE (LED_PUISSANCE=0)
-S 0x72 0x00 0x02 0x01 P --> on met en marche la sortie LED_PUISSANCE (LED_PUISSANCE=1)
-
-
-i2cget -y 2 0x39 0x01 w --> on demande la valeur de la tension de la batterie 1
-i2cget -y 2 0x39 0x02 w --> on demande la valeur de la tension de la batterie 2
-i2cget -y 2 0x39 0x03 w --> on demande la valeur de la tension de la batterie 3
-
-
-**************************************************************************************************/
+bool start_time_to_stop = false;
+bool start_time_to_power_on = false;
 
 /**
  * @brief i2c_read_data_from_buffer
  */
 void i2c_read_data_from_buffer(){
     switch(rxbuffer_tab[0]){
-    case 0x01:  // alimentation
-        if(rxbuffer_tab[1] == 0)
+    case 0x00:  // alimentation
+        switch(rxbuffer_tab[1]){
+          case 0x00:
             state = IDLE;
-        else
-            state = POWER_ON;   
-        break;
-    case 0x02:  // led power
+            break;
+          case 0x01:
+            state = POWER_ON;
+            break;
+          case 0x02:
+            time_to_stop = default_time_to_stop;  // Go to Sleep mode
+            start_time_to_stop = true;
+            state = WAIT_TO_SLEEP;
+            break;
+          default:
+            break;
+        }
+    case 0x01:  // led power
         if(rxbuffer_tab[1] == 0){
-          TMR3ON_bit = 0; // Stop timer
+          start_led_puissance = true;
           LED_PUISSANCE = 0;
         }
         else
-          TMR3ON_bit = 1; // Start timer
+          start_led_puissance = false;
         break;
-    case 0x03:
+    case 0x02:
         led_puissance_delay = rxbuffer_tab[1];
+    case 0x03:
+        default_time_to_start[0] = rxbuffer_tab[1]; // sec
+    case 0x04:
+        default_time_to_start[1] = rxbuffer_tab[1]; // min
+    case 0x05:
+        default_time_to_start[2] = rxbuffer_tab[1]; // hours
+    case 0x06:
+        default_time_to_stop = rxbuffer_tab[1]; // sec
     default:
         break;
     }
@@ -194,7 +165,7 @@ void i2c_write_data_to_buffer(unsigned short nb_tx_octet){
         SSPBUF = battery_voltage[3] >> 8;
         break;
     case 0x08:
-        SSPBUF = TMR3ON_bit;
+        SSPBUF = start_led_puissance;
     default:
         SSPBUF = 0x00;
         break;
@@ -206,13 +177,9 @@ void i2c_write_data_to_buffer(unsigned short nb_tx_octet){
  */
 void read_batteries_voltage(){
     battery_voltage[0] = ADC_Get_Sample(4);   // Get 10-bit results of AD conversion AN4 batterie 1
-    // delay_ms(5);
     battery_voltage[1] = ADC_Get_Sample(5);   // Get 10-bit results of AD conversion AN5 batterie 2
-    // delay_ms(5);
     battery_voltage[2] = ADC_Get_Sample(6);   // Get 10-bit results of AD conversion AN6 batterie 3
-    // delay_ms(5);
     battery_voltage[3] = ADC_Get_Sample(7);   // Get 10-bit results of AD conversion AN7 batterie 4
-    // delay_ms(5);
 }
 
 /**
@@ -223,10 +190,8 @@ void read_batteries_voltage(){
 void analyze_batteries_voltage(){
     battery_global_default = false;
     for(unsigned short i=0; i<4; i++){
-        if((float)(battery_voltage[i]) * QUANTUM < WARNING_LOW_VOLTAGE){
-            battery_voltage_default[i] = true;
+        if(battery_voltage[i] < WARNING_LOW_VOLTAGE)
             battery_global_default = true;
-        }
     }
 }
 
@@ -259,11 +224,11 @@ void init_i2c(){
  * Fonction d'initialisation de l'entrée d'interruption INT2 pour la détection de fermeture/ouverture
  * de l'ILS, détection du front montant (ouverture de l'ILS).
  */
-void init_io_ils(){
-    INTCON2.INTEDG2 = 0; // Interrupt on falling edge
-    INTCON3.INT2IE = 0; //Enables the INT2 external interrupt
-    INTCON3.INT2IF = 0; // INT2 External Interrupt Flag bit
-}
+// void init_io_ils(){
+//     INTCON2.INTEDG2 = 0; // Interrupt on falling edge
+//     INTCON3.INT2IE = 0; //Enables the INT2 external interrupt
+//     INTCON3.INT2IF = 0; // INT2 External Interrupt Flag bit
+// }
 
 /**
  * @brief init_timer0
@@ -272,7 +237,7 @@ void init_io_ils(){
  */
 void init_timer0(){
     //T0CON = 0x86; // TIMER0 ON time 2s
-    T0CON = 0x85; // TIMER0 ON
+    T0CON = 0x85; // TIMER0 ON (1 s)
     TMR0H = 0x0B;
     TMR0L = 0xDC;
     TMR0IE_bit = 0;
@@ -283,25 +248,25 @@ void init_timer0(){
  * Fonction d'initialisation du TIMER1
  * Prescaler 1:8; TMR1 Preload = 15536; Actual Interrupt Time : 100 ms
  */
-void init_timer1(){
-    T1CON = 0x31;
-    TMR1IF_bit = 0;
-    TMR1H = 0x77;
-    TMR1L = 0x48;
-    TMR1IE_bit = 0;
-}
+// void init_timer1(){
+//     T1CON = 0x31;
+//     TMR1IF_bit = 0;
+//     TMR1H = 0x77;
+//     TMR1L = 0x48;
+//     TMR1IE_bit = 0;
+// }
 
 /**
  * @brief init_timer2
  * Fonction d'initialisation du TIMER2
  * Prescaler 1:1; Postscaler 1:2; TMR2 Preload = 199; Actual Interrupt Time : 10 us
  */
-void init_timer2(){
-    T2CON = 0x08;
-    TMR2IE_bit = 0;
-    PR2 = 79;
-    INTCON = 0xC0;
-}
+// void init_timer2(){
+//     T2CON = 0x08;
+//     TMR2IE_bit = 0;
+//     PR2 = 79;
+//     INTCON = 0xC0;
+// }
 
 /**
  * @brief init_timer3
@@ -362,9 +327,7 @@ void main(){
 
     init_io(); // Initialisation des I/O
     init_i2c(); // Initialisation de l'I2C en esclave
-    init_io_ils(); // Initialisation de l'entrée d'interruption INT2 pour l'ILS
     init_timer0(); // Initialisation du TIMER0 toutes les 2 secondes
-    init_timer1(); // Initialisation du TIMER1 toutes les 100ms
     init_timer3(); // Initialisation du TIMER3 toutes les 100ms
 
     ADC_Init();
@@ -373,21 +336,19 @@ void main(){
     LED1 = 0;
     LED_PUISSANCE = 0; // sortie LED de puissance
     ALIM = 0; // sortie MOSFET de puissance, commande de l'alimentation
-    POWER = 0;
     battery_global_default = false;
 
     UART1_Init(9600);
 
     TMR0IE_bit = 1;  //Enable TIMER0
-    TMR1IE_bit = 1;  //Enable TIMER1
     TMR3IE_bit = 1;  //Enable TIMER3
-    INTCON3.INT2IE = 1; //Enable the INT2 external interrupt
+
     PIE1.SSPIE = 1; // Synchronous Serial Port Interrupt Enable bit
     INTCON.GIE = 1; // Global Interrupt Enable bit
     INTCON.PEIE = 1; // Peripheral Interrupt Enable bit
 
-    i = 0;
-    j = 0;
+    TMR3ON_bit = 1; // Start TIMER3
+    TMR0ON_bit = 1; // Start TIMER1
 
     while(1){
         read_batteries_voltage();
@@ -396,14 +357,15 @@ void main(){
         switch (state){
             case IDLE: // Idle state
                 ALIM = 0;
+                led_delay = 100;
 
                 if(ILS==0){ // Magnet detected
                     ils_cpt--;
-                    LED1=1;
+                    set_led_on = true;
                 }
                 else{
                     ils_cpt = ILS_CPT_TIME;
-                    LED1=0;
+                    set_led_on = false;
                     ils_removed = true;
                 }
 
@@ -413,18 +375,24 @@ void main(){
                     ils_removed = false;
                 }
                 break;
+
             case POWER_ON:
                 ALIM = 1;
+                if(battery_global_default)
+                  led_delay = 5; // 0.5 sec  
+                else
+                  led_delay = 50; // 5 sec
 
                 if(ILS==0){ // Magnet detected
                     ils_cpt--;
-                    LED1=1;
+                    set_led_on = true;
                 }
                 else{
                     ils_cpt = ILS_CPT_TIME;
-                    LED1=0;
+                    set_led_on = false;
                     ils_removed = true;
                 }
+
                 if(ils_removed && ils_cpt == 0){
                     ils_cpt = ILS_CPT_TIME;
                     state = IDLE;
@@ -432,10 +400,30 @@ void main(){
                 }
 
                 break;
-            case SLEEP:
 
+            case WAIT_TO_SLEEP:
+                ALIM = 1;
+                led_delay = 20;
+                start_time_to_stop = true;
+                if(time_to_stop==0){
+                  start_time_to_stop = false;
+                  for(size_t k=0; k<3; k++)
+                    time_to_start[k] = default_time_to_start[k];
+                  start_time_to_start = true;
+                  state = SLEEP;
+                }
+              break;
+
+            case SLEEP:
+                ALIM = 0;
+                led_delay = 600;
+                if(time_to_start[0] == 0 && time_to_start[1] == 0 && time_to_start[2] == 0){
+                  state = POWER_ON;
+                  start_time_to_start = false;
+                }
                 break;
             default:
+                state = POWER_ON;
                 break;
         }
         delay_ms(500);
@@ -476,7 +464,7 @@ void interrupt(){
             else{ // At the end of the communication
                 i2c_read_data_from_buffer();
             }
-            j = SSPBUF;
+            tmp_rx = SSPBUF;
         }
 
         PIR1.SSPIF = 0; // reset SSP interrupt flag
@@ -486,120 +474,68 @@ void interrupt(){
 
 
     /// ************************************************** //
-    /// ********************** ILS  ********************** //
-    // Interruption sur l'entrée INT2/RA2, detection d'ouverture de l'ILS
-    if(INTCON3.INT2IF == 1) {
-        INTCON3.INT2IF = 0;
-    }
-
-
-    /// ************************************************** //
     /// ********************** TIMERS  ******************* //
 
-    // Interruption du TIMER0 (1 ou 2 s)
+    // Interruption du TIMER0 (1 s) (cpt to start/stop)
     if (TMR0IF_bit){
 
-        if (POWER==1)
-          T0CON = 0x86; // TIMER0 ON time 2 secondes
-        else
-          T0CON = 0x85; // TIMER0 ON time 1 secondes
-
-        TMR0H = 0x0B;
-        TMR0L = 0xDC;
-
-        if (battery_global_default == true){
-            LED = 1;
-            T1CON.TMR1ON = 1;
+        // To Do
+      if(start_time_to_start){
+        if(time_to_start[2]>0){
+          time_to_start[2]--;
         }
-        else LED =~LED;
+        else{
+          if(time_to_start[1]>0){
+            time_to_start[1]--;
+            time_to_start[2]=59;
+          }
+          else{
+            if(time_to_start[0]>0){
+            time_to_start[0]--;
+            time_to_start[1]=59;
+            }
+          }
+        }
+      }
+
+      if(start_time_to_stop){
+        if(time_to_stop>0)
+          time_to_stop--;
+      }
 
         TMR0IF_bit = 0;
     }
 
-
-    // Interruption du TIMER1 (100 ms) => battery default
-    if (TMR1IF_bit){
-
-        TMR1H = 0x77;
-        TMR1L = 0x48;
-
-        switch (Default){
-        case 1: T1CON.TMR1ON = 0;
-            LED = 0;
-            break;
-        case 2: compteur1++;
-            if (compteur1 == 1){
-                LED = 0;
-            }
-            if (compteur1 == 2){
-                LED = 1;
-            }
-            if (compteur1 == 3){
-                LED = 0;
-                compteur1 = 0;
-                T1CON.TMR1ON = 0;
-            }
-            break;
-        case 3: compteur1++;
-            if (compteur1 == 1){
-                LED = 0;
-            }
-            if (compteur1 == 2){
-                LED = 1;
-            }
-            if (compteur1 == 3){
-                LED = 0;
-            }
-            if (compteur1 == 4){
-                LED = 1;
-            }
-            if (compteur1 == 5){
-                LED = 0;
-                compteur1 = 0;
-                T1CON.TMR1ON = 0;
-            }
-            break;
-        case 4: compteur1++;
-            if (compteur1 == 1){
-                LED = 0;
-            }
-            if (compteur1 == 2){
-                LED = 1;
-            }
-            if (compteur1 == 3){
-                LED = 0;
-            }
-            if (compteur1 == 4){
-                LED = 1;
-            }
-            if (compteur1 == 5){
-                LED = 0;
-            }
-            if (compteur1 == 6){
-                LED = 1;
-            }
-            if (compteur1 == 7){
-                LED = 0;
-                compteur1 = 0;
-                T1CON.TMR1ON = 0;
-            }
-            break;
-
-        }
-
-        TMR1IF_bit = 0;
-    }
-
-
     // Interruption du TIMER3 (Led Puissance)
     if (TMR3IF_bit){
-        cpt_led_puissance--;
-        if (cpt_led_puissance == 0){
-            LED_PUISSANCE = 1;
-            cpt_led_puissance=led_puissance_delay;
+        // LED Puissance
+        if(start_led_puissance){
+          if (cpt_led_puissance > 0){
+            LED_PUISSANCE = 0;
+            cpt_led_puissance--;
+          }
+          else{
+              LED_PUISSANCE = 1;
+              cpt_led_puissance=led_puissance_delay;
+          }
         }
-        else 
+        else{
           LED_PUISSANCE = 0;
+        }
+          
+        // LED
+        if(set_led_on) // For ILS
+          LED = 1;
+        else{
+          if (cpt_led > 0){
+            LED = 0;
+            cpt_led--;
+          }
+          else{
+            LED = 1;
+            cpt_led=led_delay;
+          }
+        }
 
         // Restart the timer
         TMR3H = 0x3C;
