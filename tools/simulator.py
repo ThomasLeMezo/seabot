df2c0f83f63e57bc4d6bd673def18d82a5456c99
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-06-04 00:30:58 +0200

    [src] Update simulator

--- tools/simulator.py
+++ tools/simulator.py
@@ -2,43 +2,49 @@ from math import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+tick_to_volume = (1.75e-3/24.0)*((0.05/2.0)**2)*np.pi
+
 g = 9.81
 rho_eau = 1025.0 # kg/m3
 
 R_tube = 0.110/2.0
 L_tube = 0.65
-V = (R_tube**2)*pi
-Cx=10.0
+V = (R_tube**2)*pi * L_tube
+Cx=1.0
 S_Cx=(R_tube**2)*pi
 
 S_piston = ((0.05/2.0)**2)*pi
 
-V_piston_max = 0.1
+V_piston_max = 200 * tick_to_volume
 
-#m = 12 # kg
+#################################################
+offset = -(40-61) * tick_to_volume
+
+#################################################
+
 m = V*rho_eau
 
 V_estime = V
 print("Masse tube equilibre = ", V*rho_eau)
-print("Coeff S_Cx*S_Cx", S_Cx*Cx)
+print("Coeff S_Cx*Cx", S_Cx*Cx)
 print("m = ", m)
 print("g*V*rho_eau/m = ", g*V*rho_eau/m)
 print("g*(V*rho_eau/m) = ", g*(V*rho_eau/m))
 print("V = ", V)
-
-tick_to_volume = (1.75e-3/24.0)*((0.05/2.0)**2)*np.pi
+print("offset = ", offset)
+print("tick_to_volume = ", tick_to_volume)
 
 def f(x, u):
 	y=np.zeros(3)
 	y[0] = x[1]
-	y[1] = g - g*((V+x[2]-0.01)*rho_eau/m) - (0.5*Cx*S_Cx*x[1]*abs(x[1])*rho_eau/m)
+	y[1] = g - g*((V+x[2]+offset)*rho_eau/m) - (0.5*Cx*S_Cx*x[1]*abs(x[1])*rho_eau/m)
 	y[2] = u
 	return y
 
 def control(d0, d, ddot, V_piston):
-	K = 50.0
+	K = 100.0
 	if(abs(d0-d)<0.2):# and abs(ddot) < 0.001):
-	 	K = 400.0
+	 	K = 1000.0
 	return 0.000001*(-(g-g*((V_estime+V_piston)*rho_eau/m)-0.5*Cx*S_Cx*ddot*abs(ddot)*rho_eau/m)+K*ddot+(d-d0))
 
 x = np.zeros(3)
@@ -49,7 +55,7 @@ result_t = []
 
 result_file = []
 
-d0 = 0.5
+d0 = 2.0
 
 dt=0.05
 t=0
@@ -75,8 +81,8 @@ for k in range(0, int(time_simulation/dt)):
 	result_x.append(x)
 	result_u.append(cmd)
 	result_t.append(t)
-	if(k%100==0 and k<200*100 and k > 10):
-		result_file.append([dx[1]+np.random.random_sample()*0.1, x[1]+np.random.random_sample()*0.01, x[2]])
+	if(k%4==0 and k<10000*4 and abs(x[0]-d0)<1.0): # 30 min : 9000*4
+		result_file.append([dx[1]+np.random.random_sample()*0.01, x[1]+np.random.random_sample()*0.001, x[2]])
 
 np.savetxt("data_dx.txt", np.transpose(result_file)[0], newline=",")
 np.savetxt("data_x1.txt", np.transpose(result_file)[1], newline=",")
