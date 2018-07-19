1b2791beddbd62f5c84a9ca9e8a1c881e7b26fa4
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-07-20 00:45:19 +0200

    [src] Update

--- src/seabot_mission/src/mission.cpp
+++ src/seabot_mission/src/mission.cpp
@@ -7,30 +7,10 @@
 #include "seabot_power_driver/Battery.h"
 
 using namespace std;
-double battery_limit = 10.0;
-bool battery_limit_reached = false;
-
-void batteries_callback(const seabot_power_driver::Battery::ConstPtr& msg){
-  if(msg->battery1 < battery_limit
-     && msg->battery2 < battery_limit
-     && msg->battery3 < battery_limit
-     && msg->battery4 < battery_limit){
-    battery_limit_reached = true;
-  }
-  else{
-    battery_limit_reached = false;
-  }
-
-}
 
 int main(int argc, char *argv[]){
   ros::init(argc, argv, "mission_node");
   ros::NodeHandle n;
-
-  //  piston_position = rospy.Publisher('/regulation/depth_set_point', DepthPose, queue_size=1)
-
-  // Subscriber
-  ros::Subscriber batteries_sub = n.subscribe("/fusion/batteries", 1, batteries_callback);
 
   // Publisher
   ros::Publisher waypoint_pub = n.advertise<seabot_mission::Waypoint>("set_point", 1);
@@ -41,7 +21,6 @@ int main(int argc, char *argv[]){
   const double frequency = n_private.param<double>("frequency", 1.0);
   const string mission_file_name = n_private.param<string>("mission_file_name", "mission_test.xml");
   const string mission_path = n_private.param<string>("mission_path", "~");
-  battery_limit = n_private.param<double>("battery_limit", 10.0);
 
   ros::Rate loop_rate(frequency);
   SeabotMission m(mission_path);
@@ -56,18 +35,11 @@ int main(int argc, char *argv[]){
 
     m.compute_command(north, east, depth, ratio);
 
-    if(!battery_limit_reached){
-      waypoint_msg.depth_only = m.is_depth_only();
-      waypoint_msg.depth = depth;
-      waypoint_msg.north = north;
-      waypoint_msg.east = east;
-      waypoint_msg.mission_enable = m.is_mission_enable();
-    }
-    else{
-      waypoint_msg.depth_only = true;
-      waypoint_msg.depth = 0.0;
-      waypoint_msg.mission_enable = m.is_mission_enable();
-    }
+    waypoint_msg.depth_only = m.is_depth_only();
+    waypoint_msg.depth = depth;
+    waypoint_msg.north = north;
+    waypoint_msg.east = east;
+    waypoint_msg.mission_enable = m.is_mission_enable();
     waypoint_pub.publish(waypoint_msg);
 
     loop_rate.sleep();
--- src/seabot_safety/src/main.cpp
+++ src/seabot_safety/src/main.cpp
@@ -9,7 +9,9 @@
 #include <seabot_mission/Waypoint.h>
 #include <std_srvs/Trigger.h>
 #include <std_srvs/SetBool.h>
+#include <std_srvs/Empty.h>
 #include <pressure_bme280_driver/Bme280Data.h>
+#include <seabot_power_driver/Battery.h>
 
 #include <cmath>
 
@@ -29,9 +31,26 @@ ros::WallTime time_pressure_limit_reached;
 // Flash
 bool flash_is_enable = false;
 
+// Batteries
+double battery_limit = 10.0;
+bool battery_limit_reached = false;
+
 ros::ServiceClient service_zero_depth;
 ros::ServiceClient service_flash_enable;
 ros::ServiceClient service_emergency;
+ros::ServiceClient service_sleep;
+
+void batteries_callback(const seabot_power_driver::Battery::ConstPtr& msg){
+  if(msg->battery1 < battery_limit
+     && msg->battery2 < battery_limit
+     && msg->battery3 < battery_limit
+     && msg->battery4 < battery_limit){
+    battery_limit_reached = true;
+  }
+  else{
+    battery_limit_reached = false;
+  }
+}
 
 void piston_callback(const seabot_piston_driver::PistonState::ConstPtr& msg){
   piston_position = msg->position;
@@ -62,10 +81,17 @@ void call_zero_depth(){
   }
 }
 
-void call_emergency(const bool &val){
+void call_emergency_depth(const bool &val){
   std_srvs::SetBool srv;
   if (!service_emergency.call(srv)){
     ROS_ERROR("[Safety] Failed to call emergency");
+  }
+}
+
+void call_sleep(){
+  std_srvs::Empty srv;
+  if (!service_sleep.call(srv)){
+    ROS_ERROR("[Safety] Failed to call sleep");
   }
 }
 
@@ -101,11 +127,14 @@ int main(int argc, char *argv[]){
   const double limit_depth_flash_enable = n_private.param<double>("limit_depth_flash_enable", 0.5);
   const double time_before_pressure_emergency = n_private.param<double>("time_before_pressure_emergency", 3.0);
 
+  battery_limit = n_private.param<double>("battery_limit", 10.0);
+
   pressure_limit = n_private.param<double>("pressure_limit", 6.2);
 
   // Subscriber
   ros::Subscriber depth_sub = n.subscribe("/fusion/depth", 1, depth_callback);
   ros::Subscriber state_sub = n.subscribe("/driver/piston/state", 1, piston_callback);
+  ros::Subscriber batteries_sub = n.subscribe("/fusion/batteries", 1, batteries_callback);
 
   // Service
   ROS_INFO("[Safety] Wait for zero depth service from fusion");
@@ -122,12 +151,17 @@ int main(int argc, char *argv[]){
   ros::service::waitForService("/regulation/emergency");
   service_emergency = n.serviceClient<std_srvs::SetBool>("/regulation/emergency");
 
+  ROS_INFO("[Safety] Wait for sleep service from power_driver");
+  ros::service::waitForService("/driver/power/sleep_mode");
+  service_sleep = n.serviceClient<std_srvs::Empty>("/driver/power/sleep_mode");
+
   // Server
   ros::ServiceServer server_reset_limit_depth = n.advertiseService("reset_limit_depth", reset_limit_depth);
 
   ros::Rate loop_rate(frequency);
 
   // Main regulation loop
+  ROS_INFO("[Safety] Start safety loop");
   while (ros::ok()){
     ros::spinOnce();
 
@@ -135,16 +169,27 @@ int main(int argc, char *argv[]){
     if(piston_position == 0 && depth < max_depth_reset_zero && abs(velocity) < max_speed_reset_zero)
       call_zero_depth();
 
-    // Flash
+    // Flash at surface
     if(depth < limit_depth_flash_enable)
       call_flash_enable(true);
     else
       call_flash_enable(false);
 
     // Depth limit
-    if(is_pressure_limit_reached && (ros::WallTime::now()-time_pressure_limit_reached).toSec()>time_before_pressure_emergency)
-      call_emergency(true);
+    if(is_pressure_limit_reached && (ros::WallTime::now()-time_pressure_limit_reached).toSec()>time_before_pressure_emergency){
+      ROS_WARN("[Safety] Limit depth detected");
+      call_emergency_depth(true);
+    }
 
+    // Batteries
+    if(battery_limit_reached){
+      ROS_WARN("[Safety] Batteries limit detected");
+      call_emergency_depth(true);
+      // ToDo : shutdown ? => launch with iridium sleep ?
+    }
+
+    // Internal pressure
+    // ToDo
 
     loop_rate.sleep();
   }
