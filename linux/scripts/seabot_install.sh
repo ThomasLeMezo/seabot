5628b133300a480199f5087823a5e2c6c8c079dc
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-08-20 18:11:35 +0200

    [src] Update scripts

--- linux/scripts/seabot_install.sh
+++ linux/scripts/seabot_install.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gpsd gpsd-clients gpsd-dbg ntp ntp-doc ntpstat socat nmap zsh git ssh cron cronutils screen i2c-tools htop pps-tools
+sudo apt install -y gpsd gpsd-clients gpsd-dbg ntp ntp-doc ntpstat socat nmap zsh git ssh cron cronutils screen i2c-tools htop pps-tools libgps-dev minicom
 
 # Note :
 # - Do not install ntpdate
@@ -15,11 +15,11 @@ git clone https://github.com/ThomasLeMezo/workspaceFlotteur.git
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --keyserver-options http-proxy=http://squid:3128 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-kinetic-ros-base
+sudo apt-get install -y ros-kinetic-ros-base
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
 source ~/.zshrc
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 echo "source ~/workspaceFlotteur/devel/setup.zsh" >> ~/.zshrc
