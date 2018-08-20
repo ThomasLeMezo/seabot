68ae02c1d11a09c013900e70ae50e3be421b7415
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-08-20 16:11:16 +0200

    [src] Update linux install files

---
+++ linux/seabot_install.sh
@@ -0,0 +1,9 @@
+sudo apt install -y gpsd gpsd-clients gpsd-dbg ntp ntp-doc ntpstat socat nmap zsh git ssh cron cronutils screen i2c-tools htop pps-tools
+
+# Note :
+# - Do not install ntpdate
+
+chsh -s /bin/zsh
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
