69f6eed5d1e482610e79cde04fa46b50289868ed
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-10-22 23:07:42 +0200

    [src] Update ais

--- tools/aislib/aistest.py
+++ tools/aislib/aistest.py
@@ -1,9 +1,5 @@
-import aislib
-
-import time
-import serial
+import aislib, time, serial, yaml, datetime
 from math import *
-import yaml
 from os.path import expanduser
 from pyproj import Proj, transform
 
@@ -27,29 +23,50 @@ while 1:
     
     east = data_yaml['east']
     north = data_yaml['north']
-    imei = int(data_yaml['file_name'][15-9:15])
+    imei = int(data_yaml['file_name'][15-6:15])
+    current_waypoint = data_yaml['current_waypoint']
+    ts = int(data_yaml['time'])
+    msg_date = datetime.datetime.utcfromtimestamp(ts)
+
     # print(msg_id)
+
+    ship_name = "SEABOT"
+    if(imei == 981770):
+        ship_name += "1"
+    elif(imei == 396100):
+        ship_name += "2"
+    elif(imei == 392110):
+        ship_name += "3"
 
     # current_waypoint = data_yaml['current_waypoint']
     longitude,latitude = transform(inProj,outProj,east, north)
     # print(longitude, latitude)
-    aismsg = aislib.AISPositionReportMessage(
+    aismsg1 = aislib.AISPositionReportMessage(
         mmsi = imei,
-        status = 8,
-        sog = min(int(round(data_yaml['gnss_speed']*19.4384)), 4095), # vitesse en dixieme de noeuds
-        pa = 1, # position accuracy (1bit)
-        # lat = int(round(floor(latitude)*60+(latitude-floor(latitude))*100.)), # en 1/10 000 de minutes, Ouest negatif
-        # lon = int(round(floor(longitude)*60+(longitude-floor(longitude))*100.)),
+        sog = min(int(round(data_yaml['gnss_speed']*19.4384)), 1022), # vitesse en dixieme de noeuds
+        pa = 1,
         lat = int(round(latitude*60e4)),
         lon = int(round(longitude*60e4)),
-        cog = min(int(data_yaml['gnss_heading']*10.), 3600), # max 3600
-        ts = 40, # max 511
-        raim = 1,
-        comm_state = 1   
+        cog = min(int(data_yaml['gnss_heading']*10.), 3599)#, # max 3600
+        # ts = msg_date.second
     )
-    ais = aislib.AIS(aismsg)
-    payload = ais.build_payload(False)
-    # print payload
-    ser.write(payload+'\n')
+
+    ais1 = aislib.AIS(aismsg1)
+    payload1 = ais1.build_payload(False)
+    ser.write(payload1+'\n')
+
+    aismsg2 = aislib.AISStaticAndVoyageReportMessage(
+            shipname = ship_name,
+            mmsi = imei,
+            epfd = 3,
+            month = msg_date.month,
+            day = msg_date.day,
+            hour = msg_date.hour,
+            minute = msg_date.minute,
+            destination = "WAYPOINT " + str(current_waypoint)
+        )
+    ais2 = aislib.AIS(aismsg2)
+    payload2 = ais2.build_payload(False)
+    ser.write(payload2+'\n')
 
     time.sleep(1.0)
