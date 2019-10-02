23721f9e1b8ae96f55f6268bcd56d153e6feabbc
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2019-10-02 12:50:54 +0200

    [log,qgis] Update log + add boat gnss in Qgis

--- tools/qgis/seabot/seabot_dockwidget.py
+++ tools/qgis/seabot/seabot_dockwidget.py
@@ -28,6 +28,7 @@ from PyQt5 import QtGui, QtWidgets, uic
 from PyQt5.QtCore import pyqtSignal, QTimer
 
 from .seabotLayerLivePosition import SeabotLayerLivePosition
+from .boatLayerLivePosition import BoatLayerLivePosition
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'seabot_dockwidget_base.ui'))
@@ -36,11 +37,13 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 class SeabotDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
 
     closingPlugin = pyqtSignal()
-    timer = QTimer()
+    timer_seabot = QTimer()
+    timer_boat = QTimer()
     flag = False
     count = 0
 
     layerLivePosition = SeabotLayerLivePosition()
+    boatLivePosition = BoatLayerLivePosition()
 
     def __init__(self, parent=None):
         """Constructor."""
@@ -54,29 +57,44 @@ class SeabotDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         print(self.__dir__)
         self.setupUi(self)
 
-        self.timer.timeout.connect(self.process)
-        self.timer.setInterval(5000)
-        # self.timer.start()
+        self.timer_seabot.timeout.connect(self.process_seabot)
+        self.timer_seabot.setInterval(5000)
 
-        self.pushButton.clicked.connect(self.enable_timer)
+        self.timer_boat.timeout.connect(self.process_boat)
+        self.timer_boat.setInterval(1000)
+
+        self.pushButton.clicked.connect(self.enable_timer_seabot)
+        self.pushButtonBoat.clicked.connect(self.enable_timer_boat)
 
     def closeEvent(self, event):
-        self.timer.stop()
+        self.timer_seabot.stop()
+        self.timer_boat.stop()
         self.pushButton.setChecked(False)
         self.closingPlugin.emit()
         event.accept()
 
-    def enable_timer(self):
+    def enable_timer_seabot(self):
         if(self.pushButton.isChecked()):
-            self.timer.start()
+            self.timer_seabot.start()
         else:
-            self.timer.stop()
+            self.timer_seabot.stop()
 
-    def process(self):
+    def enable_timer_boat(self):
+        if(self.pushButtonBoat.isChecked()):
+            self.boatLivePosition.start()
+            self.timer_boat.start()
+        else:
+            self.timer_boat.stop()
+            self.boatLivePosition.stop()
+
+    def process_seabot(self):
         if(self.layerLivePosition.update()):
             self.label.setText("Connected - " + str(self.count))
             self.count += 1
         else:
             self.label.setText("Error")
             self.count = 0
+
+    def process_boat(self):
+        self.boatLivePosition.update()
         
