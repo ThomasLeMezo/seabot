da3e8f22c0e6f869e203c3ec0eb38db015f98a09
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2019-10-28 22:27:18 +0100

    [src] Update visualization tools

--- tools/rosbag/vibes_plot_depth.py
+++ tools/rosbag/vibes_plot_depth.py
@@ -12,7 +12,40 @@ from math import *
 if(len(sys.argv)<2):
 	sys.exit(0)
 
-load_bag(sys.argv[1])
+pistonStateData = PistonStateData()
+pistonSetPointData = PistonSetPointData()
+imuData = ImuData()
+magData = MagData()
+eulerData = EulerData()
+pistonVelocityData = PistonVelocityData()
+pistonDistanceData = PistonDistanceData()
+pistonSpeedData = PistonSpeedData()
+batteryData = BatteryData()
+sensorExtData = SensorExtData()
+sensorIntData = SensorIntData()
+engineData = EngineData()
+engineCmdData = EngineCmdData()
+fixData = FixData()
+temperatureData = TemperatureData()
+batteryFusionData = BatteryFusionData()
+sensorIntFusionData = SensorIntFusionData()
+depthFusionData = DepthFusionData()
+poseFusionData = PoseFusionData()
+kalmanData = KalmanData()
+regulationData = RegulationData()
+regulationHeadingData = RegulationHeadingData()
+regulationHeadingSetPointData = RegulationHeadingSetPointData()
+missionData = MissionData()
+safetyData = SafetyData()
+safetyDebugData = SafetyDebugData()
+iridiumStatusData = IridiumStatusData()
+iridiumSessionData = IridiumSessionData()
+regulationWaypointData = RegulationWaypointData()
+
+
+filename = sys.argv[1]
+load_bag(filename, pistonStateData, pistonSetPointData, imuData, magData, eulerData, pistonVelocityData, pistonDistanceData, pistonSpeedData, batteryData, sensorExtData, sensorIntData, engineData, engineCmdData, fixData, temperatureData, batteryFusionData, sensorIntFusionData, depthFusionData, poseFusionData, kalmanData, regulationData, regulationHeadingData, regulationHeadingSetPointData, missionData, safetyData, safetyDebugData, iridiumStatusData, iridiumSessionData, regulationWaypointData)
+
 g = 9.81
 m = 9.045
 rho = 1025.0
@@ -40,10 +73,10 @@ def newFigure(name):
 	vibes.axisLimits(x_min-offset, x_max+offset, y_min-offset, y_max+offset)
 	vibes.drawBox(x_min-offset, x_max+offset, y_min-offset, y_max+offset, "white[white]")
 
-data = np.transpose(np.vstack([time_fusion_depth, fusion_depth]))
-data_ref = np.transpose(np.vstack([time_mission, mission_depth]))
+data = np.transpose(np.vstack([depthFusionData.time, depthFusionData.depth]))
+data_ref = np.transpose(np.vstack([missionData.time, missionData.depth]))
 
-data_piston = np.transpose(np.vstack([time_piston_state, np.array(piston_state_position)*tick_to_volume*1e6]))
+data_piston = np.transpose(np.vstack([pistonStateData.time, pistonStateData.position*tick_to_volume*1e6]))
 
 vibes.beginDrawing()
 newFigure("Depth regulation")
@@ -62,30 +95,30 @@ vibes.saveImage(file_directory + 'float_regulation_piston.svg')
 
 
 ###
-ti=280*5
-tf=3500*5
-data_chi = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_chi[ti:tf])*1e6]))
-data_offset = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_offset[ti:tf])*1e6]))
-# data_chi_cov = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_cov_chi[ti:tf])]))
-# data_offset_cov = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_cov_offset[ti:tf])]))
+# ti=280*5
+# tf=3500*5
+# data_chi = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_chi[ti:tf])*1e6]))
+# data_offset = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_offset[ti:tf])*1e6]))
+# # data_chi_cov = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_cov_chi[ti:tf])]))
+# # data_offset_cov = np.transpose(np.vstack([time_kalman[ti:tf], np.array(kalman_cov_offset[ti:tf])]))
 
-#t = 280 -> 3640
-x_min = ti/5.-1. # for display
-x_max = tf/5.+1
+# #t = 280 -> 3640
+# x_min = ti/5.-1. # for display
+# x_max = tf/5.+1
 
-y_min = 240.*tick_to_volume*1e6
-y_max = 400.*tick_to_volume*1e6
-newFigure("Offset")
-vibes.drawLine(data_offset.tolist(), "black")
-# vibes.axisAuto()
-vibes.saveImage(file_directory + 'float_offset.svg')
+# y_min = 240.*tick_to_volume*1e6
+# y_max = 400.*tick_to_volume*1e6
+# newFigure("Offset")
+# vibes.drawLine(data_offset.tolist(), "black")
+# # vibes.axisAuto()
+# vibes.saveImage(file_directory + 'float_offset.svg')
 
-y_min = -60.*tick_to_volume*1e6
-y_max = 70.*tick_to_volume*1e6
-newFigure("Chi")
-vibes.drawLine(data_chi.tolist(), "black")
-# vibes.axisAuto()
-vibes.saveImage(file_directory + 'float_chi.svg')
+# y_min = -60.*tick_to_volume*1e6
+# y_max = 70.*tick_to_volume*1e6
+# newFigure("Chi")
+# vibes.drawLine(data_chi.tolist(), "black")
+# # vibes.axisAuto()
+# vibes.saveImage(file_directory + 'float_chi.svg')
 
 # newFigure("Offset_Cov")
 # y_min = -60.*tick_to_volume*1e6
